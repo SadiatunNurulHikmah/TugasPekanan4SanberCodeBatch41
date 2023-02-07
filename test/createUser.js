da9ce80d1d3a237f02c9bb36eb268f1cc4a4f55b
SadiatunNurulHikmah <diah.hikmah@gmail.com>
2023-02-07 19:48:08 +0700

    update createUser & addUnit

--- test/createUser.js
+++ test/createUser.js
@@ -4,22 +4,27 @@ const { expect } = require('chai');
 const request = require('supertest');
 
 
-const baseUrl = 'https://kasirdemo.belajarqa.com';
+const baseUrl = 'https://kasir-api.belajarqa.com';
+const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjA0NjNmZDQzLTk3MWMtNDY0Yi04NzllLTdkMzQ3OTllMTkwOCIsImNvbXBhbnlJZCI6ImY2MGUxNTFjLTZhOTMtNGI4My1iNDBhLWM0Yjc1MzA3OTkwZSIsImlhdCI6MTY3NTc2MDEzM30.MLqY9_WxeOJVqVtCSdczua9_3Kvg-WIAs9fl4v2QNe4';
+
 
 describe('Create a New User in One Company', function(){
     const response = request(baseUrl) //baseUrl
         .post('/users') //endpoint
+        .auth(TOKEN, {type:"bearer"} )
         .send({
             "name": "kasir-serbaguna",
             "email": "user@example.com",
             "password": "jiasda2321@"
          })
+  
 
-    // Cek Response Status 200
-    it('response status is 200', async () =>{ 
+    // Cek Response Status 201 Created
+    it('response status is 201', async () =>{ 
         console.log((await response).status)
+        console.log((await response).headers)
         console.log((await response).body)
-        expect((await response).status).to.equal(200)
+        expect((await response).status).to.equal(201)
     })
 
     /*it('message is equal to Pet ID', async () =>{ 
