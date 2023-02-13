1b04076033fd3233f5c8c5511c6bf9a9c470dcb0
SadiatunNurulHikmah <diah.hikmah@gmail.com>
2023-02-13 18:28:49 +0700

    mod getUser

--- test/getUser.js
+++ test/getUser.js
@@ -10,7 +10,9 @@ const baseUrl = 'https://kasir-api.belajarqa.com';
 // email = diah.hikmah@gmail.com
 // password = tokodiah
 const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjA0NjNmZDQzLTk3MWMtNDY0Yi04NzllLTdkMzQ3OTllMTkwOCIsImNvbXBhbnlJZCI6ImY2MGUxNTFjLTZhOTMtNGI4My1iNDBhLWM0Yjc1MzA3OTkwZSIsImlhdCI6MTY3NTc2MDEzM30.MLqY9_WxeOJVqVtCSdczua9_3Kvg-WIAs9fl4v2QNe4';
+
 /*
+//Get User List
 const searchUser = 'minimarket'
 const searchPage = 1
 
@@ -37,6 +39,8 @@ describe('Get User List', function(){
 
 })
 */
+
+// Get User Detail
 const userId = '7c4f1bb5-8783-4b72-9a6c-04cb26912a9e=-=-=';
 
 describe('Get User Detail By Id', function(){
@@ -45,6 +49,7 @@ describe('Get User Detail By Id', function(){
         .auth(TOKEN, {type:"bearer"} )
   
 /*
+    // Positive Case
     // Cek Response Status 200 OK
     it('response status is 200 OK', async () =>{ 
         console.log((await response).status)
@@ -57,6 +62,7 @@ describe('Get User Detail By Id', function(){
         expect((await response).body.status).to.equal("success")
     }) 
 */
+    // Negative Case
     // Response Status 404 Not Found
     it('Response Status is 404 Not Found', async () =>{ 
         console.log((await response).status)
