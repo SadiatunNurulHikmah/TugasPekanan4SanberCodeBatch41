43bfa1994a83c1e515baa55861c8ebe7e8bcd954
SadiatunNurulHikmah <diah.hikmah@gmail.com>
2023-02-13 16:30:30 +0700

    mod getUser

--- test/getUser.js
+++ test/getUser.js
@@ -1,4 +1,4 @@
-// get a list of users associated with user’s token
+// get about one user details
 
 const { expect } = require('chai');
 const request = require('supertest');
@@ -10,7 +10,8 @@ const baseUrl = 'https://kasir-api.belajarqa.com';
 // email = diah.hikmah@gmail.com
 // password = tokodiah
 const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjA0NjNmZDQzLTk3MWMtNDY0Yi04NzllLTdkMzQ3OTllMTkwOCIsImNvbXBhbnlJZCI6ImY2MGUxNTFjLTZhOTMtNGI4My1iNDBhLWM0Yjc1MzA3OTkwZSIsImlhdCI6MTY3NTc2MDEzM30.MLqY9_WxeOJVqVtCSdczua9_3Kvg-WIAs9fl4v2QNe4';
-const searchUser = "minimarket"
+/*
+const searchUser = 'minimarket'
 const searchPage = 1
 
 describe('Get User List', function(){
@@ -24,6 +25,29 @@ describe('Get User List', function(){
   
     // Response Status 200 OK
     it('Response Status is 200 OK', async () =>{ 
+        console.log((await response).status)
+        expect((await response).status).to.equal(200)
+    })
+
+    // Cek isi Response Body
+    it('Response Body is OK', async () =>{ 
+        console.log((await response).body.data.users)
+        expect((await response).body.status).to.equal("success")
+    })
+
+})
+*/
+const userId = '7c4f1bb5-8783-4b72-9a6c-04cb26912a9e=-=-=';
+
+describe('Get User Detail By Id', function(){
+    const response = request(baseUrl) //baseUrl
+        .get('/users/' + userId) //endpoint
+        .auth(TOKEN, {type:"bearer"} )
+  
+/*
+    // Cek Response Status 200 OK
+    it('response status is 200 OK', async () =>{ 
+        console.log((await response).status)
         expect((await response).status).to.equal(200)
     })
 
@@ -31,8 +55,18 @@ describe('Get User List', function(){
     it('Response Body is OK', async () =>{ 
         console.log((await response).body)
         expect((await response).body.status).to.equal("success")
-    })
+    }) 
+*/
+    // Response Status 404 Not Found
+    it('Response Status is 404 Not Found', async () =>{ 
+        console.log((await response).status)
+        expect((await response).status).to.equal(404)
+    }) 
 
+    // Cek isi Response Body
+    it('Response Body for Status Fail', async () =>{ 
+        console.log((await response).body)
+        expect((await response).body.status).to.equal("fail")
+        expect((await response).body.message).to.equal("id tidak valid")
+    }) 
 })
-
-
